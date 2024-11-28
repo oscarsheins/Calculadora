4e31ee0afceb0c303b56f6cfb89eb07c0d589d40
oscar <ocordom078@g.educaand.es>
2024-11-28 11:23:07 +0100

    CalculadoraFinalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
+  numero1 : number = 0;
+  numero2 : number = 0;
+  resultado : number = 0;
+
+  sumar(): void {
+
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(){
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar(){
+    this.resultado = this.numero1 * this.numero2;
+  }
+  dividir(){
+
+    if ( this.numero2 !== 0){
+      this.resultado = this.numero1 / this.numero2;
+    } else {
+      this.resultado = NaN;
+    }
+
+  }
 }
